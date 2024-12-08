6f4166e99e82e13387d7d74426438d5a9714ed5c
peterstrongg <peterstrongdev@outlook.com>
2024-12-07 22:57:44 -0500

    Removing commented code

--- ws/main.go
+++ ws/main.go
@@ -28,46 +28,6 @@ type serverMessage struct {
 	PlayerBInput string `json:"playerBInput"`
 }
 
-// type gameSession struct {
-// 	SessionId string
-// 	PlayerA   *websocket.Conn
-// 	PlayerB   *websocket.Conn
-// }
-
-// type SessionsContainer struct {
-// 	mu           sync.Mutex
-// 	gameSessions []gameSession
-// }
-
-// var sc SessionsContainer
-
-// func (sc *SessionsContainer) findGameSession(id string) *gameSession {
-// 	sc.mu.Lock()
-// 	defer sc.mu.Unlock()
-
-// 	for i := 0; i < len(sc.gameSessions); i++ {
-// 		if sc.gameSessions[i].SessionId == id {
-// 			return &sc.gameSessions[i]
-// 		}
-// 	}
-// 	var gs gameSession
-// 	gs.SessionId = id
-// 	sc.gameSessions = append(sc.gameSessions, gs)
-// 	return &sc.gameSessions[len(sc.gameSessions)-1]
-// }
-
-// func (sc *SessionsContainer) deleteGameSession(id string) {
-// 	sc.mu.Lock()
-// 	defer sc.mu.Unlock()
-
-// 	for i := 0; i < len(sc.gameSessions); i++ {
-// 		if sc.gameSessions[i].SessionId == id {
-// 			sc.gameSessions = append(sc.gameSessions[:i], sc.gameSessions[i+1:]...)
-// 			return
-// 		}
-// 	}
-// }
-
 func main() {
 	http.HandleFunc("/ws/play/", playGame)
 	err := http.ListenAndServe(":8081", nil)
